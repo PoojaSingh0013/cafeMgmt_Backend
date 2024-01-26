dfacaba2a3f2e9cd4c44bda7d028393bc52173a7
Pooja <poojasingh8710013@gmail.com>
2024-01-26 11:00:37 +0530

    Initial commit

---
+++ cafeMgmt/src/main/java/com/cafeMgmt/CafeMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.cafeMgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeMgmtApplication.class, args);
+	}
+
+}
---
+++ cafeMgmt/src/test/java/com/cafeMgmt/CafeMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafeMgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
